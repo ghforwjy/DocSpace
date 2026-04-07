a1e26873ccc84f09cc4e5b03b45c427c80273725
ghforwjy <179537@qq.com>
2026-04-07 13:35:49 +0800

    chore: 删除临时脚本文件

--- run_migration.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect -f
-set timeout 300
-spawn bash -c "cd /mnt/e/mycode/DocSpace/buildtools/install/docker && newgrp docker << 'ENDGRP'
-docker compose -f migration-runner.yml run --rm onlyoffice-migration-runner
-ENDGRP"
-expect "password"
-send "666888\r"
-interact
--- start_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo pkill dockerd 2>/dev/null || true
-sleep 2
-sudo HTTP_PROXY=http://192.168.31.165:7897 HTTPS_PROXY=http://192.168.31.165:7897 dockerd > /tmp/dockerd.log 2>&1 &
-sleep 5
-docker info 2>/dev/null | grep -i -A 2 proxy
